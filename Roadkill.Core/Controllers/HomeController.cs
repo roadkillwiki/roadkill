4a99e4c033f78bdc41d868fe8dc339fbec4dcd32
Chris Small <mrshrinkray@gmail.com>
2012-06-21 22:38:24 +0100

    Closed small hole so locked homepage-tagged pages are always used first, switched JsVars to using a view

--- Roadkill.Core/Controllers/HomeController.cs
+++ Roadkill.Core/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace Roadkill.Core.Controllers
 	/// Provides functionality that is common through the site.
 	/// </summary>
 	public class HomeController : ControllerBase
-    {
+	{
 		/// <summary>
 		/// Display the homepage/mainpage. If no page has been tagged with the 'homepage' tag,
 		/// then a dummy PageSummary is put in its place.
@@ -24,7 +24,15 @@ namespace Roadkill.Core.Controllers
 		public ActionResult Index()
 		{
 			PageManager manager = new PageManager();
-			PageSummary summary = manager.FindByTag("homepage").FirstOrDefault();
+
+			// Get the first locked homepage
+			PageSummary summary = manager.FindByTag("homepage").FirstOrDefault(h => h.IsLocked);
+			if (summary == null)
+			{
+				// Look for a none-locked page as a fallback
+				summary = manager.FindByTag("homepage").FirstOrDefault();
+			}
+
 			if (summary == null)
 			{
 				summary = new PageSummary();
@@ -53,54 +61,13 @@ namespace Roadkill.Core.Controllers
 		}
 
 		/// <summary>
-		/// Returns a string containing Javascript 'constants' for the site. If the user is logged in, 
-		/// additional variables are returned that are used by the edit page.</summary>
+		/// Returns Javascript 'constants' for the site. If the user is logged in, 
+		/// additional variables are returned that are used by the edit page.
+		/// </summary>
 		public ActionResult GlobalJsVars()
 		{
-			UrlHelper helper = new UrlHelper(HttpContext.Request.RequestContext);
-
-			StringBuilder builder = new StringBuilder();
-			builder.AppendLine(string.Format("var ROADKILL_CORESCRIPTPATH = '{0}';", helper.Content("~/Assets/Scripts/")));
-			builder.AppendLine(string.Format("var ROADKILL_THEMEPATH =  '{0}/';", Url.Content(RoadkillSettings.ThemePath)));
-
-			if (RoadkillContext.Current.IsLoggedIn)
-			{
-				// Edit page constants
-				builder.AppendLine(string.Format("var ROADKILL_TAGAJAXURL = '{0}/';", helper.Action("AllTagsAsJson", "Pages")));
-				builder.AppendLine(string.Format("var ROADKILL_PREVIEWURL = '{0}/';", helper.Action("GetPreview", "Pages")));
-				builder.AppendLine(string.Format("var ROADKILL_MARKUPTYPE = '{0}';", RoadkillSettings.MarkupType));
-				builder.AppendLine(string.Format("var ROADKILL_WIKIMARKUPHELP = '{0}';", helper.Action(RoadkillSettings.MarkupType + "Reference", "Help")));
-
-				// File manager constants
-				builder.AppendLine(string.Format("var ROADKILL_FILEMANAGERURL = '{0}';", helper.Action("Index", "Files")));
-				builder.AppendLine(string.Format("var ROADKILL_FILETREE_URL =  '{0}/';", Url.Action("Folder","Files")));
-				builder.AppendLine(string.Format("var ROADKILL_FILETREE_PATHNAME_URL =  '{0}/';", Url.Action("GetPath", "Files")));
-				builder.AppendLine(string.Format("var ROADKILL_ATTACHMENTSPATH = '{0}';", Url.Content(RoadkillSettings.AttachmentsFolder)));
-
-				// Tokens for the edit toolbar
-				MarkupConverter converter = new MarkupConverter();
-				IMarkupParser parser = converter.Parser;
-				builder.AppendLine(string.Format("var ROADKILL_EDIT_BOLD_TOKEN = \"{0}\";", parser.BoldToken));
-				builder.AppendLine(string.Format("var ROADKILL_EDIT_ITALIC_TOKEN = \"{0}\";", parser.ItalicToken));
-				builder.AppendLine(string.Format("var ROADKILL_EDIT_UNDERLINE_TOKEN = \"{0}\";", parser.UnderlineToken));
-				builder.AppendLine(string.Format("var ROADKILL_EDIT_LINK_STARTTOKEN = \"{0}\";", parser.LinkStartToken));
-				builder.AppendLine(string.Format("var ROADKILL_EDIT_LINK_ENDTOKEN = \"{0}\";", parser.LinkEndToken));
-				builder.AppendLine(string.Format("var ROADKILL_EDIT_IMAGE_STARTTOKEN = \"{0}\";", parser.ImageStartToken));
-				builder.AppendLine(string.Format("var ROADKILL_EDIT_IMAGE_ENDTOKEN = \"{0}\";", parser.ImageEndToken));
-				builder.AppendLine(string.Format("var ROADKILL_EDIT_NUMBERLIST_TOKEN = \"{0}\";", parser.NumberedListToken));
-				builder.AppendLine(string.Format("var ROADKILL_EDIT_BULLETLIST_TOKEN = \"{0}\";", parser.BulletListToken));
-				builder.AppendLine(string.Format("var ROADKILL_EDIT_HEADING_TOKEN = \"{0}\";", parser.HeadingToken));
-
-				if (RoadkillContext.Current.IsAdmin)
-				{
-					// User management constants for the settings->user page
-					builder.AppendLine(string.Format("var ROADKILL_ADDADMIN_FORMACTION = \"{0}\";", helper.Action("AddAdmin","Settings")));
-					builder.AppendLine(string.Format("var ROADKILL_ADDEDITOR_FORMACTION = \"{0}\";", helper.Action("AddEditor","Settings")));
-					builder.AppendLine(string.Format("var ROADKILL_EDITUSER_FORMACTION = \"{0}\";", helper.Action("EditUser","Settings")));
-				}
-			}
-
-			return Content(builder.ToString(), "text/javascript");
+			Response.ContentType = "text/javascript";
+			return PartialView();
 		}
-    }
+	}
 }
