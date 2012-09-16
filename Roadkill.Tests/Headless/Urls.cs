4d9c1bae71b4e7f17353ae575509f10beebd47d3
Chris S <mrshrinkray@gmail.com>
2012-09-16 12:51:37 +0100

    Added simplebrowser to tests, sql ce test database for dev work + web.config for this

---
+++ Roadkill.Tests/Headless/Urls.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Roadkill.Tests.Headless
+{
+	public class Settings
+	{
+		public static string HeadlessUrl { get; private set; }
+
+		static Settings()
+		{
+			HeadlessUrl = "http://roadkill.apphb.com";
+		}
+	}
+}
