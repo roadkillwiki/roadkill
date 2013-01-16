e49c25ec8c8067a4bb0cefc42659e94a73602784
Chris S <mrshrinkray@gmail.com>
2013-01-16 21:48:03 +0000

    More PagesController tests

---
+++ src/Roadkill.Tests/Unit/Stubs/RoadkillContextStub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Roadkill.Core;
+
+namespace Roadkill.Tests.Unit
+{
+	public class RoadkillContextStub : IRoadkillContext
+	{
+		public string CurrentUser { get; set; }
+		public string CurrentUsername { get; set; }
+		public bool IsAdmin { get; set; }
+		public bool IsContentPage { get; set; }
+		public bool IsEditor { get; set; }
+		public bool IsLoggedIn { get; set; }
+		public PageSummary Page { get; set; }
+	}
+}
