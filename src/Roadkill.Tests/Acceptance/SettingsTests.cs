920d5581351d5939e6244bff6e9293971b039a5e
Chris S <mrshrinkray@gmail.com>
2013-03-15 22:34:29 +0000

    Updated acceptance tests SQL CE file

--- src/Roadkill.Tests/Acceptance/SettingsTests.cs
+++ src/Roadkill.Tests/Acceptance/SettingsTests.cs
@@ -26,10 +26,10 @@ namespace Roadkill.Tests.Acceptance
 
 			// Assert
 			Assert.That(Driver.ElementValue("#SiteName"), Is.EqualTo("Acceptance Tests"));
-			Assert.That(Driver.ElementValue("#SiteUrl"), Is.EqualTo("http://localhost:57025"));
+			Assert.That(Driver.ElementValue("#SiteUrl"), Is.EqualTo("http://localhost:9876"));
 			Assert.That(Driver.ElementValue("#ConnectionString"), Is.StringStarting(@"Data Source=|DataDirectory|\roadkill-acceptancetests.sdf"));
-			Assert.That(Driver.ElementValue("#RecaptchaPrivateKey"), Is.EqualTo("Recaptcha Private Key"));
-			Assert.That(Driver.ElementValue("#RecaptchaPublicKey"), Is.EqualTo("Recaptcha Public Key"));
+			Assert.That(Driver.ElementValue("#RecaptchaPrivateKey"), Is.EqualTo("recaptcha-private-key"));
+			Assert.That(Driver.ElementValue("#RecaptchaPublicKey"), Is.EqualTo("recaptcha-public-key"));
 			Assert.That(Driver.ElementValue("#EditorRoleName"), Is.EqualTo("Editor"));
 			Assert.That(Driver.ElementValue("#AdminRoleName"), Is.EqualTo("Admin"));
 			Assert.That(Driver.ElementValue("#AttachmentsFolder"), Is.EqualTo(@"C:\temp\roadkillattachments"));
