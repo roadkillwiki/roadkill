07913dacb18dd578a22311cf4e5af2d4403a7c7b
Chris S <mrshrinkray@gmail.com>
2013-03-16 01:46:09 +0000

    Documentation as MD

--- docs/source/combine.ps1
+++ docs/source/combine.ps1
@@ -1,1 +1,3 @@
 Get-ChildItem *.md | Sort-Object { [regex]::Replace($_.Name, '\d+', { $args[0].Value.PadLeft(20) }) }| gc | out-file combinedfile.txt
+ren combinedfile.txt readme.md
+move -Force readme.md ..\..\readme.md
