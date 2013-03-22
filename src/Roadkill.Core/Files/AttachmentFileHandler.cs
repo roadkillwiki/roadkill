1abcadc49daef248fe6b119a7193ddedd32e96c3
Chris S <mrshrinkray@gmail.com>
2013-03-22 22:08:06 +0000

    Fixed 304/200s and cache of attachment files

--- src/Roadkill.Core/Files/AttachmentFileHandler.cs
+++ src/Roadkill.Core/Files/AttachmentFileHandler.cs
@@ -64,17 +64,19 @@ namespace Roadkill.Core.Files
 					TimeSpan expires = TimeSpan.FromDays(28);
 					context.Response.Cache.SetLastModifiedFromFileDependencies();
 					context.Response.Cache.SetETagFromFileDependencies();
-					context.Response.Cache.SetExpires(DateTime.UtcNow.Add(expires));
-					context.Response.Cache.SetMaxAge(expires);
 					context.Response.Cache.SetCacheability(HttpCacheability.Public);
 
-					int status = 304;
+					int status = 200;
 					if (context.Request.Headers["If-Modified-Since"] != null)
 					{
-						DateTime modifiedSince = DateTime.Now;
-						if (DateTime.TryParse(context.Request.Headers["If-Modified-Since"], out modifiedSince))
+						status = 304;
+						DateTime modifiedSinceDate = DateTime.UtcNow;
+						if (DateTime.TryParse(context.Request.Headers["If-Modified-Since"], out modifiedSinceDate))
 						{
-							if (info.LastWriteTime >= modifiedSince)
+							modifiedSinceDate = modifiedSinceDate.ToUniversalTime();
+							DateTime fileDate = info.LastWriteTimeUtc;
+							DateTime lastWriteTime = new DateTime(fileDate.Year, fileDate.Month, fileDate.Day, fileDate.Hour, fileDate.Minute, fileDate.Second, 0, DateTimeKind.Utc);
+							if (lastWriteTime != modifiedSinceDate)
 								status = 200;
 						}
 					}
