10ad36525cb15fb0988e0485459fa6b93d95c528
Chris S. <mrshrinkray@gmail.com>
2013-03-30 11:25:17 +0000

    IDepedencyResolve refactor 2

---
+++ src/Roadkill.Core/Controllers/Attributes/IInjectedAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Roadkill.Core.Configuration;
+using StructureMap.Attributes;
+
+namespace Roadkill.Core
+{
+	/// <summary>
+	/// Defines an Attribute that has its property values setter injected by Structuremap.
+	/// </summary>
+	public interface IInjectedAttribute
+	{
+		[SetterProperty]
+		IConfigurationContainer Configuration { get; set; }
+
+		[SetterProperty]
+		IRoadkillContext Context { get; set; }
+
+		[SetterProperty]
+		UserManager UserManager { get; set; }
+
+		[SetterProperty]
+		PageManager PageManager { get; set; }
+	}
+}
