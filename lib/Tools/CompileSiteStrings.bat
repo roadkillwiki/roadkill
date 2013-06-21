352ce0b0db2a24559832dc8d050c2526f434af6c
Chris S. <mrshrinkray@gmail.com>
2013-06-21 02:34:56 +0100

    Missing addremove files

--- lib/Tools/CompileSiteStrings.bat
+++
@@ -1,4 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\VsDevCmd.bat
-cd ..\..\src\Roadkill.Core\Localization\Text
-resgen /compile SiteStrings.txt,SiteStrings.resx
-move /Y SiteStrings.resx ..\Resx\
