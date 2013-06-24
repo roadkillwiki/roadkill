5b64ea3f7c0e7214e2593a1131cbcccbb740e1fb
Chris S. <mrshrinkray@gmail.com>
2013-06-24 23:19:29 +0100

    Build script

--- build.ps1
+++ build.ps1
@@ -2,7 +2,8 @@
 
 # ---- Add the tool paths to our path
 $runtimeDir = [System.Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()
-$env:Path = $env:Path + $runtimeDir
+$env:Path = $env:Path +";"+ $runtimeDir
+Write-host "Added $runtimeDir to path"
 
 # ---- Build the solution using the Download target
 msbuild roadkill.sln "/p:Configuration=Download"
