31eab18c92291d836340d1281d3360f5b83952e4
Chris S. <mrshrinkray@gmail.com>
2013-10-16 14:24:47 +0100

    Fixed broken hg ren, re-added mvc folder

---
+++ src/Roadkill.Core/Mvc/Setup/Routing.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Roadkill.Core.Mvc
+{
+	public class Routing
+	{
+		public static void Register(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+			routes.IgnoreRoute("favicon.ico");
+
+			// For the jQuery ajax file manager
+			routes.MapLowercaseRoute(
+				"FileFolder",
+				"Files/Folder/{dir}",
+				new { controller = "Files", action = "Folder", dir = UrlParameter.Optional }
+			);
+
+			// 404 error
+			routes.MapLowercaseRoute(
+				"NotFound",
+				"wiki/notfound",
+				new { controller = "Wiki", action = "NotFound", id = UrlParameter.Optional }
+			);
+
+			// 500 error
+			routes.MapLowercaseRoute(
+				"ServerError",
+				"wiki/servererror",
+				new { controller = "Wiki", action = "ServerError", id = UrlParameter.Optional }
+			);
+
+			// The default way of getting to a page: "/wiki/123/page-title"
+			routes.MapLowercaseRoute(
+				"Wiki",
+				"Wiki/{id}/{title}",
+				new { controller = "Wiki", action = "Index", title = UrlParameter.Optional }
+			);
+
+			// Don't lowercase pages that use Base64
+			routes.MapRoute(
+				"Pages",
+				"pages/byuser/{id}/{encoded}",
+				new { controller = "Pages", action = "ByUser", title = UrlParameter.Optional }
+			);
+
+			// Default
+			routes.MapLowercaseRoute(
+				"Default", // Route name
+				"{controller}/{action}/{id}", // URL with parameters
+				new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+			);
+		}
+	}
+}
