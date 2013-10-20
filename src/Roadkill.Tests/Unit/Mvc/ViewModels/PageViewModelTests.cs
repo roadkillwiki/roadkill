83c247f2734ce1dde372a70d438404d05987aef5
Chris S. <mrshrinkray@gmail.com>
2013-10-20 23:19:33 +0100

    More model unit tests

---
+++ src/Roadkill.Tests/Unit/Mvc/ViewModels/PageViewModelTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Roadkill.Core.Configuration;
+using Roadkill.Core.Mvc.ViewModels;
+
+namespace Roadkill.Tests.Unit
+{
+	[TestFixture]
+	[Category("Unit")]
+	public class PageViewModelTests
+	{
+		[Test]
+		public void Constructor_Should_Fill_Properties()
+		{
+			// Arrange
+
+			// Act
+			PageViewModel model = new PageViewModel();			
+
+			// Assert
+			
+		}
+
+		// Content
+		// IsNew
+		// RawTags
+
+		// CommaDelimitedTags
+		// SpaceDelimitedTags
+		// ParseRawTags
+		// ParseTags
+		// VerifyRawTags
+	}
+}
