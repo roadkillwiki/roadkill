9ff01f0bf38f03fda92f0c752218c6687efb723a
Chris S. <mrshrinkray@gmail.com>
2013-10-20 22:16:07 +0100

    Model unit tests

--- src/Roadkill.Core/Mvc/ViewModels/PageHistoryViewModel.cs
+++ src/Roadkill.Core/Mvc/ViewModels/PageHistoryViewModel.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Roadkill.Core.Database;
 
 namespace Roadkill.Core.Mvc.ViewModels
 {
 	/// <summary>
 	/// Provides summary data for a page's version history.
 	/// </summary>
-	public class HistoryViewModel
+	public class PageHistoryViewModel
 	{
 		/// <summary>
 		/// The unique Guid for the version.
@@ -31,7 +32,7 @@ namespace Roadkill.Core.Mvc.ViewModels
 		public string EditedBy { get; set; }
 
 		/// <summary>
-		/// When this version was created.
+		/// When this version was created. This should always be a UTC time / date.
 		/// </summary>
 		public DateTime EditedOn { get; set; }
 
@@ -51,5 +52,27 @@ namespace Roadkill.Core.Mvc.ViewModels
 		/// Whether the page can only be edited by administrators. This disables the "revert" behaviour.
 		/// </summary>
 		public bool IsPageAdminOnly { get; set; }
+
+		public PageHistoryViewModel()
+		{
+			EditedOn = DateTime.UtcNow;
+		}
+
+		/// <summary>
+		/// Fills this instance using the <see cref="PageContent"/> object.
+		/// </summary>
+		/// <exception cref="ArgumentNullException">PageContent.Page should not be null.</exception>
+		public PageHistoryViewModel(PageContent pageContent)
+		{
+			if (pageContent.Page == null)
+				throw new ArgumentNullException("pageContent.Page should not be null");
+
+			Id = pageContent.Id;
+			PageId = pageContent.Page.Id;
+			EditedBy = pageContent.EditedBy;
+			EditedOn = pageContent.EditedOn;
+			VersionNumber = pageContent.VersionNumber;
+			IsPageAdminOnly = pageContent.Page.IsLocked;
+		}
 	}
 }
---
+++ src/Roadkill.Tests/Unit/Mvc/ViewModels/TagViewModelTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Roadkill.Core.Configuration;
+using Roadkill.Core.Mvc.ViewModels;
+using Roadkill.Core.Plugins;
+using Roadkill.Tests.Unit.StubsAndMocks;
+
+namespace Roadkill.Tests.Unit
+{
+	[TestFixture]
+	[Category("Unit")]
+	public class TagViewModelTests
+	{
+		[Test]
+		public void Constructor_Should_Fill_Properties()
+		{
+			// Arrange + Act
+			TagViewModel model = new TagViewModel("tag1");	
+
+			// Assert
+			Assert.That(model.Name, Is.EqualTo("tag1"));
+			Assert.That(model.Count, Is.EqualTo(1));
+		}
+	}
+}
