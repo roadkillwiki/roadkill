da4e56a8d90c4ab00c0dc3474bfee922de244c62
Chris S. <mrshrinkray@gmail.com>
2013-10-23 21:18:17 +0100

    Added connectionStrings.config + tidied build scripts

--- devbuild.ps1
+++ devbuild.ps1
@@ -26,9 +26,9 @@ $env:Path = $env:Path + ";" +$runtimeDir
 $env:Path = $env:Path + ";C:\Program Files (x86)\IIS\Microsoft Web Deploy V3"
 $env:Path = $env:Path + ";C:\Program Files\7-Zip"
 
-# ---- Make sure the web.config/roadkill.config file is the devbuild template one
-copy -Force lib\Configs\web.devbuild.config src\Roadkill.site\web.config
-copy -Force lib\Configs\roadkill.devbuild.config src\Roadkill.site\roadkill.config
+# ---- Make sure the roadkill.config,connectionstrings.config files are the download template one
+copy -Force lib\Configs\roadkill.download.config src\Roadkill.site\roadkill.config
+copy -Force lib\Configs\connectionStrings.config src\Roadkill.site\connectionStrings.config
 
 # ---- Build the solution using the Download target
 msbuild roadkill.sln "/p:Configuration=Download;DeployOnBuild=True;PackageAsSingleFile=False;AutoParameterizationWebConfigConnectionStrings=false;outdir=deploytemp\;OutputPath=bin\debug"
