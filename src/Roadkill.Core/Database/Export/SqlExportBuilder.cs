97701a876165ebc10821b14548826f438f111182
Chris S. <mrshrinkray@gmail.com>
2013-11-09 18:08:22 +0000

    SQL export tests

--- src/Roadkill.Core/Database/Export/SqlExportBuilder.cs
+++ src/Roadkill.Core/Database/Export/SqlExportBuilder.cs
@@ -2,20 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Roadkill.Core.Configuration;
 using Roadkill.Core.Logging;
+using Roadkill.Core.Plugins;
 
 namespace Roadkill.Core.Database.Export
 {
 	public class SqlExportBuilder
 	{
 		private IRepository _repository;
+		private IPluginFactory _pluginFactory;
 
-		public SqlExportBuilder(IRepository repository)
+		public bool IncludePages { get; set; }
+		public bool IncludeConfiguration { get; set; }
+
+		public SqlExportBuilder(IRepository repository, IPluginFactory pluginFactory)
 		{
 			if (repository == null)
 				throw new ArgumentNullException("repository");
 
+			if (pluginFactory == null)
+				throw new ArgumentNullException("pluginFactory");
+
 			_repository = repository;
+			_pluginFactory = pluginFactory;
+			IncludePages = true;
+			IncludeConfiguration = true;
 		}
 
 		public string Export()
@@ -25,22 +37,49 @@ namespace Roadkill.Core.Database.Export
 				IEnumerable<User> users = _repository.FindAllAdmins().Union(_repository.FindAllEditors());
 				IEnumerable<Page> pages = _repository.AllPages();
 				IEnumerable<PageContent> pageContent = _repository.AllPageContents();
+				IEnumerable<SiteConfigurationRow> configurationRows = GetSiteConfigurationRows();
 
 				// The order of the SQL is important - users should come before pages, pages before content.
-				string usersSql = string.Join(Environment.NewLine, users.Select(x => GetUsersInsertSql(x)).ToArray());
-				string pagesSql = string.Join(Environment.NewLine, pages.Select(x => GetPagesInsertSql(x)).ToArray());
-				string pageContentSql = string.Join(Environment.NewLine, pageContent.Select(x => GetPageContentInsertSql(x)).ToArray());
+				string usersSql = GetSqlLines<User>(users, GetUsersInsertSql);
+				string pagesSql = GetSqlLines<Page>(pages, GetPagesInsertSql);
+				string pageContentSql = GetSqlLines<PageContent>(pageContent, GetPageContentInsertSql);
+				string configurationSql = GetSqlLines<SiteConfigurationRow>(configurationRows, GetSiteConfigurationInsertSql);
 
 				StringBuilder sqlBuilder = new StringBuilder();
 				sqlBuilder.AppendLine("-- You will need to enable identity inserts for your chosen db before running this Script, for example in SQL Server:");
 				sqlBuilder.AppendLine("-- SET IDENTITY_INSERT roadkill_pages ON;");
-				sqlBuilder.AppendLine(usersSql);
-				
-				if (!string.IsNullOrWhiteSpace(pagesSql))
-					sqlBuilder.AppendLine(pagesSql);
 
-				if (!string.IsNullOrWhiteSpace(pageContentSql))
-					sqlBuilder.AppendLine(pageContentSql);
+				// Always output the users, it's used by the pages
+				sqlBuilder.AppendLine();
+				sqlBuilder.AppendLine("-- Users");
+				sqlBuilder.AppendLine(usersSql);
+
+				if (IncludePages)
+				{
+					if (!string.IsNullOrWhiteSpace(pagesSql))
+					{
+						sqlBuilder.AppendLine();
+						sqlBuilder.AppendLine("-- Pages");
+						sqlBuilder.AppendLine(pagesSql);
+					}
+
+					if (!string.IsNullOrWhiteSpace(pageContentSql))
+					{
+						sqlBuilder.AppendLine();
+						sqlBuilder.AppendLine("-- Pages contents");
+						sqlBuilder.AppendLine(pageContentSql);
+					}
+				}
+
+				if (IncludeConfiguration)
+				{
+					if (!string.IsNullOrWhiteSpace(configurationSql))
+					{
+						sqlBuilder.AppendLine();
+						sqlBuilder.AppendLine("-- Configuration");
+						sqlBuilder.AppendLine(configurationSql);
+					}
+				}
 
 				Log.Debug("Sql export successfully written: \n\n{0}", sqlBuilder);
 				return sqlBuilder.ToString();
@@ -52,8 +91,55 @@ namespace Roadkill.Core.Database.Export
 			}
 		}
 
-		public string GetPagesInsertSql(Page page)
+		/// <summary>
+		/// This avoids a bit of repetition, but is harder to read. Takes a  list of objects,
+		/// and calls a method to generate the sql for each item in the list. The sql is then 
+		/// joined to together with a newline.
+		/// </summary>
+		private string GetSqlLines<T>(IEnumerable<T> items, Func<T, string> sqlInsertScriptMethod)
+		{	
+			string[] sqlRows = items.Select(x => sqlInsertScriptMethod(x)).ToArray();
+			return string.Join(Environment.NewLine, sqlRows);
+		}
+
+		private IEnumerable<SiteConfigurationRow> GetSiteConfigurationRows()
 		{
+			List<SiteConfigurationRow> configurationRows = new List<SiteConfigurationRow>();
+			
+			// Turn the main SiteSettings into a row
+			SiteSettings settings = _repository.GetSiteSettings();
+			SiteConfigurationRow row = new SiteConfigurationRow()
+			{
+				Id = SiteSettings.SiteSettingsId,
+				Version = ApplicationSettings.FileVersion,
+				Json = settings.GetJson()
+			};
+
+			configurationRows.Add(row);
+
+			// Turn all plugin settings into rows
+			IEnumerable<TextPlugin> plugins = _pluginFactory.GetTextPlugins();
+
+			foreach (TextPlugin plugin in plugins)
+			{
+				row = new SiteConfigurationRow()
+				{
+					Id = plugin.DatabaseId,
+					Version = plugin.Version,
+					Json = plugin.GetSettingsJson()
+				};
+
+				configurationRows.Add(row);
+			}
+
+			return configurationRows;
+		}
+
+		internal string GetPagesInsertSql(Page page)
+		{
+			if (page == null)
+				return "";
+
 			string sql = "INSERT INTO roadkill_pages (id, title, createdby, createdon, modifiedby, modifiedon, tags, islocked) VALUES (";
 			sql += string.Format("'{0}',", page.Id);
 			sql += string.Format("'{0}',", page.Title.ReplaceSingleQuotes());
@@ -69,8 +155,11 @@ namespace Roadkill.Core.Database.Export
 			return sql;
 		}
 
-		public string GetPageContentInsertSql(PageContent content)
+		internal string GetPageContentInsertSql(PageContent content)
 		{
+			if (content == null)
+				return "";
+
 			string sql = "INSERT INTO roadkill_pagecontent (id, pageid, text, editedby, editedon, versionnumber) VALUES (";
 			sql += string.Format("'{0}',", content.Id);
 			sql += string.Format("'{0}',", content.Page.Id);
@@ -84,8 +173,11 @@ namespace Roadkill.Core.Database.Export
 			return sql;
 		}
 
-		public string GetUsersInsertSql(User user)
+		internal string GetUsersInsertSql(User user)
 		{
+			if (user == null)
+				return "";
+
 			string sql = "INSERT INTO roadkill_users (id, activationkey, email, firstname, iseditor, isadmin, isactivated, lastname, password, passwordresetkey, salt, username) VALUES (";
 			sql += string.Format("'{0}',", user.Id);
 			sql += string.Format("'{0}',", user.ActivationKey);
@@ -103,6 +195,28 @@ namespace Roadkill.Core.Database.Export
 			sql += ");";
 
 			return sql;
+		}
+
+		internal string GetSiteConfigurationInsertSql(SiteConfigurationRow row)
+		{
+			if (row == null)
+				return "";
+
+			string sql = "INSERT INTO roadkill_siteconfiguration (id, version, content) VALUES (";
+			sql += string.Format("'{0}',", row.Id);
+			sql += string.Format("'{0}',", row.Version);
+			sql += string.Format("'{0}'", row.Json.ReplaceSingleQuotes());
+
+			sql += ");";
+
+			return sql;
+		}
+
+		internal class SiteConfigurationRow
+		{
+			public Guid Id { get; set; }
+			public string Version { get; set; }
+			public string Json { get; set; }
 		}
 	}
 
