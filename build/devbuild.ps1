c14bb5638882a2b6230e83d962233d39e798aaca
Chris S. <mrshrinkray@gmail.com>
2015-01-25 22:26:43 +0000

    Removed roadkill js files, del hgignore, gitignore, devbuild update

--- build/devbuild.ps1
+++ build/devbuild.ps1
@@ -64,8 +64,7 @@ Remove-Item -Force -Recurse _WEBSITE
 # ---- Commit to builds repository
 CD ..\roadkillbuilds
 $commitMessage = "Dev build " + [DateTime]::Now.toString()
-hg addremove
-hg commit -m $commitMessage
-hg push
+git commit -m $commitMessage
+git push origin master
 
 "DEV BUILD Complete."
