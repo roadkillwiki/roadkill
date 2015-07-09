506b1ad0b49f38b45b5ecd278c1eee0b1981ad49
Chris S <mrshrinkray@gmail.com>
2015-07-09 22:27:08 +0100

    connectionstrings.ps1

--- build/appveyor/set-connectionstrings.ps1
+++ build/appveyor/set-connectionstrings.ps1
@@ -1,7 +1,7 @@
 $appVeyorConfig = "lib\Configs\connectionStrings.appveyor.config";
 $destConfigPath  = "lib\Configs\connectionStrings.dev.config";
 
-Copy-Item -Path $appVeyorConfig -Destination $devConfigPath -Force
+Copy-Item -Path $appVeyorConfig -Destination $destConfigPath -Force
 
 Write-Host "Contents of lib\Configs\connectionStrings.dev.config :"
 Write-Host "------------------------------------------------------"
