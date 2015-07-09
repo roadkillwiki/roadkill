5f35447970647ecf12c9fedc7f3b8e70172d69aa
Chris S <mrshrinkray@gmail.com>
2015-07-09 01:15:16 +0100

    appveyor

--- build/appveyor/set-connectionstrings.ps1
+++ build/appveyor/set-connectionstrings.ps1
@@ -1,11 +1,11 @@
 $devConfigPath = "lib\Configs\connectionStrings.dev.config";
 $testsSetupPath = "src\Roadkill.Tests\Setup\SqlExpressSetup.cs";
 
-$connectionStrings = Get-Content -Path $devConfigPath
+$connectionStrings = Get-Content -Path $devConfigPath;
 $hardcodedCsharp = Get-Content -Path $testsSetupPath;
 
-$originalDbConnection = "Server=(local);Integrated Security=true;Connect Timeout=5;database=Roadkill"
-$appveyorDbConnection = "Server=(local)\SQL2012SP1;Database=master;User ID=sa;Password=Password12!"
+$originalDbConnection = "Server=(local);Integrated Security=true;Connect Timeout=5;database=Roadkill";
+$appveyorDbConnection = "Server=(local)\SQL2012SP1;Database=master;User ID=sa;Password=Password12!";
 
 $connectionStrings.replace($originalDbConnection, $appveyorDbConnection) | Out-File $devConfigPath;
 $hardcodedCsharp.replace($originalDbConnection, $appveyorDbConnection) | Out-File $testsSetupPath;
