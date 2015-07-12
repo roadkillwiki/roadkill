35ebc460fcac1472b684a840e45fe6805be8e10d
Chris S <mrshrinkray@gmail.com>
2015-07-13 00:31:17 +0100

    Appveyor: updated mongodb script

--- build/appveyor/install-mongodb.ps1
+++ build/appveyor/install-mongodb.ps1
@@ -1,37 +1,19 @@
-$mongoDbPath = "C:\MongoDB" 
-$mongoDbConfigPath = "$mongoDbPath\mongod.cfg"
-$url = "http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-latest.zip" 
-$zipFile = "$mongoDbPath\mongo.zip" 
-$unzippedFolderContent ="$mongoDbPath\mongodb-win32-x86_64-2008plus-ssl-latest"
+$msiPath = "$($env:USERPROFILE)\mongodb-win32-x86_64-2008plus-ssl-3.0.4-signed.msi"
+(New-Object Net.WebClient).DownloadFile('https://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-3.0.4-signed.msi', $msiPath)
+cmd /c start /wait msiexec /q /i $msiPath INSTALLLOCATION=C:\mongodb ADDLOCAL="all"
+del $msiPath
  
-if ((Test-Path -path $mongoDbPath) -eq $True) 
-{ 
-  write-host "Seems you already installed MongoDB"
-	exit 
-}
+mkdir c:\mongodb\data\db | Out-Null
+mkdir c:\mongodb\log | Out-Null
  
-md $mongoDbPath 
-md "$mongoDbPath\log" 
-md "$mongoDbPath\data" 
-md "$mongoDbPath\data\db"
+'systemLog:
+    destination: file
+    path: c:\mongodb\log\mongod.log
+storage:
+    dbPath: c:\mongodb\data\db' | Out-File C:\mongodb\mongod.cfg -Encoding utf8
  
-[System.IO.File]::AppendAllText("$mongoDbConfigPath", "dbpath=C:\MongoDB\data\db`r`n")
-[System.IO.File]::AppendAllText("$mongoDbConfigPath", "logpath=C:\MongoDB\log\mongo.log`r`n")
-[System.IO.File]::AppendAllText("$mongoDbConfigPath", "smallfiles=true`r`n")
-[System.IO.File]::AppendAllText("$mongoDbConfigPath", "noprealloc=true`r`n")
+cmd /c start /wait sc create MongoDB binPath= "C:\mongodb\bin\mongod.exe --service --config=C:\mongodb\mongod.cfg" DisplayName= "MongoDB" start= "demand"
  
-$webClient = New-Object System.Net.WebClient 
-$webClient.DownloadFile($url,$zipFile)
+& c:\mongodb\bin\mongod --version
  
-$shellApp = New-Object -com shell.application 
-$destination = $shellApp.namespace($mongoDbPath) 
-$destination.Copyhere($shellApp.namespace($zipFile).items())
- 
-Copy-Item "$unzippedFolderContent\*" $mongoDbPath -recurse
- 
-Remove-Item $unzippedFolderContent -recurse -force 
-Remove-Item $zipFile -recurse -force
- 
-& $mongoDBPath\bin\mongod.exe --config $mongoDbConfigPath --install
- 
-& net start mongodb
+Start-Service mongodb
