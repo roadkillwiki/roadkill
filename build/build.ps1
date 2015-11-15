9edf8d85f32e98a64fe3fa216f7061b3130af4c4
Chris S <mrshrinkray@gmail.com>
2015-11-15 02:16:47 +0000

    Warning level for builds back to 1

--- build/build.ps1
+++ build/build.ps1
@@ -26,7 +26,7 @@ nuget restore $solutionFile
 # Build the sln file
 Write-Host "Building $solutionFile." -ForegroundColor Green
 
-& $msbuild $solutionFile /p:Configuration=$configuration /p:RunOctoPack=true /p:OctoPackPublishPackageToFileShare="$octopackTargetDir" /p:Platform=$platform /target:Build /verbosity:minimal 
+& $msbuild $solutionFile /p:Configuration=$configuration /p:RunOctoPack=true /p:OctoPackPublishPackageToFileShare="$octopackTargetDir" /p:Platform=$platform /target:Build /verbosity:minimal /p:WarningLevel=1
 if ($LastExitCode -ne 0)
 {
     throw "Building solution failed."
