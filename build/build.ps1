a72620b7e0e8161000f067087d95bc690d33aaf9
Chris S <mrshrinkray@gmail.com>
2015-11-15 00:37:39 +0000

    Removed packages folder, added Coverall support and new build script.

--- build/build.ps1
+++ build/build.ps1
@@ -1,9 +1,37 @@
-# Basic msbuild script
+# ===============================================================================
+# 1. Performs a nuget restore
+# 2. Runs msbuild.exe with octopack output pointing to $octopackTargetDir
+#
+# Note: This script should be run from the root directory, not from the build folder.
+# ===============================================================================
+$ErrorActionPreference = "Stop"
 
-# Add the tool paths to our path
-$runtimeDir = [System.Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()
-$env:Path = $env:Path +";"+ $runtimeDir
-Write-host "Added $runtimeDir to path"
+$solutionFile      = "Roadkill.sln"
+$configuration     = "Release"
+$platform          = "Any CPU"
+$msbuild           = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
 
-# Build the solution
-msbuild ..\roadkill.sln "/p:Configuration=Release,VisualStudioVersion=14.0" /t:Rebuild
+$root              = $PSScriptRoot;
+$octopackTargetDir = "$root\_deploymentOutput\";
+
+if (!(Test-Path $msbuild))
+{
+    $msbuild = "C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
+}
+
+# Nuget restore
+Write-Host "Performing Nuget restore" -ForegroundColor Green
+nuget restore $solutionFile
+
+# Build the sln file
+Write-Host "Building $solutionFile." -ForegroundColor Green
+
+& $msbuild $solutionFile /p:Configuration=$configuration /p:RunOctoPack=true /p:OctoPackPublishPackageToFileShare="$octopackTargetDir" /p:Platform=$platform /target:Build /verbosity:minimal 
+if ($LastExitCode -ne 0)
+{
+    throw "Building solution failed."
+}
+else
+{
+    Write-Host "Building solution complete."-ForegroundColor Green
+}
--- packages/Chutzpah.2.5.0/tools/ctz.bat
+++
@@ -1,6 +0,0 @@
-﻿@echo off
-
-rem Alias for Chutzpah.Console.exe
-rem Usage: ctz somefile.js
-
-%~dp0chutzpah.console.exe %*
