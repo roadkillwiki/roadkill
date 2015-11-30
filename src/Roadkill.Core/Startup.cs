5789769d9ddb3ba404975abb3ff2220a10bc72c0
Chris S <mrshrinkray@gmail.com>
2015-11-30 18:14:43 +0000

    Move to OWIN using appSettings for the Startup class definition.

---
+++ src/Roadkill.Core/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Owin;
+using Roadkill.Core.Configuration;
+using Roadkill.Core.DI;
+using Roadkill.Core.Logging;
+using Roadkill.Core.Mvc;
+
+namespace Roadkill.Core
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			// Get the settings from the web.config
+			ConfigReaderWriter configReader = new FullTrustConfigReaderWriter("");
+			ApplicationSettings applicationSettings = configReader.GetApplicationSettings();
+
+			// Configure StructureMap dependencies
+			var iocSetup = new DependencyManager(applicationSettings);
+			iocSetup.Configure();
+			iocSetup.ConfigureMvc();
+
+			// Logging
+			Log.ConfigureLogging(applicationSettings);
+
+			// Filters
+			GlobalFilters.Filters.Add(new HandleErrorAttribute());
+
+			// Areas are used for:
+			// - Site settings (for a cleaner view structure)
+			// This should be called before the other routes, for some reason.
+			AreaRegistration.RegisterAllAreas();
+
+			// Register routes and JS/CSS bundles
+			Routing.RegisterApi(GlobalConfiguration.Configuration);
+			Routing.Register(RouteTable.Routes);
+
+			// Custom view engine registration (to add directory search paths for Theme views)
+			ExtendedRazorViewEngine.Register();
+
+			app.UseWebApi(new HttpConfiguration());
+
+			Log.Information("Application started");
+		}
+	}
+}
--- src/Roadkill.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Roadkill.Core.RoadkillApplication" Language="C#" %>
