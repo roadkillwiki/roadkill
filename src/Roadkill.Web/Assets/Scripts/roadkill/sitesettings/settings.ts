d4a27fc62fd0055b7b4f7a7e15fbe1384d245973
Chris S <mrshrinkray@gmail.com>
2017-04-05 13:37:09 +0100

    Another TS2 fix for href

--- src/Roadkill.Web/Assets/Scripts/roadkill/sitesettings/settings.ts
+++ src/Roadkill.Web/Assets/Scripts/roadkill/sitesettings/settings.ts
@@ -121,7 +121,7 @@ module Roadkill.Web.Admin
 				// Logged out since the call was made
 				if (errorThrown.message.indexOf("unexpected character") !== -1)
 				{
-					window.location = window.location;
+					window.location.href = window.location.href;
 				}
 				else
 				{
